29a45d1c6f4b55476da1e0838b1b196341a50f5d
Ninezel <ninez2025@gmail.com>
2022-12-03 22:18:26 +0000

    New additions

---
+++ src/DynInit.java
@@ -0,0 +1,11 @@
+// Demonstration of a dynamic initialisation
+public class DynInit {
+    public static void main(String[] args) {
+        double a = 3.0, b = 4.0;
+
+        // c is dynamically initialised
+        double c = Math.sqrt(a * a + b * b);
+
+        System.out.println("Hypotenuse is: " + c);
+    }
+}
