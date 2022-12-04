c48ea0eb514d3ff3e6c6b97d91c7dff8f2fa4311
Ninezel <ninez2025@gmail.com>
2022-12-04 13:48:20 +0000

    New additions

---
+++ src/ScopeErr.java
@@ -0,0 +1,9 @@
+// This code will not compile
+public class ScopeErr {
+    public static void main(String[] args) {
+        int bar = 1;
+        {
+            // int bar = 2; // Compile-time error - bar already defined
+        }
+    }
+}
