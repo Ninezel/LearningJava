1ab07bd0fd6503e21de15b1c094506d3845330cf
Ninezel <ninez2025@gmail.com>
2022-12-04 17:05:17 +0000

    New additions

--- src/TwoDAgain.java
+++ src/TwoDAgain.java
@@ -1,7 +1,7 @@
 // Manually allocate differing size second dimensions
 public class TwoDAgain {
     public static void main(String[] args) {
-        int[] [] twoD = new int[4][];
+        int[][] twoD = new int[4][];
         twoD[0] = new int[1];
         twoD[1] = new int[2];
         twoD[2] = new int[3];
--- src/TwoDArray.java
+++ src/TwoDArray.java
@@ -1,7 +1,7 @@
 // Demonstration of a two-dimensional array
 public class TwoDArray {
     public static void main(String[] args) {
-        int [] [] twoD= new int[4] [5];
+        int [][] twoD= new int[4][5];
         int i, j, k = 0;
 
         for(i=0; i<4; i++)
