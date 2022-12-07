6ef585c3da0bfd68870bd87f7c8ee1ba22faed7c
Ninezel <ninez2025@gmail.com>
2022-12-07 10:08:54 +0000

    New additions

---
+++ src/MultByTwo.java
@@ -0,0 +1,12 @@
+// Left shifting as a quick way to multiply by 2.
+public class MultByTwo {
+    public static void main(String[] args) {
+        int i;
+        int num = 0xFFFFFFE;
+
+        for(i = 0; i < num; i++) {
+            num = num << 1;
+            System.out.println(num);
+        }
+    }
+}
